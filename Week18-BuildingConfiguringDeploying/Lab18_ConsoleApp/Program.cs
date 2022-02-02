a355e3d7abc099e70ed4a0069b08142634d3b854
Dawid Korba <P2577974@my365.dmu.ac.uk>
2022-02-02 12:38:27 +0000

    week 18 complete

---
+++ Week18-BuildingConfiguringDeploying/Lab18_ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+using System.Configuration;
+
+namespace Lab18_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Debug.WriteLine("Write something to the output window... blablablabla");
+            Console.WriteLine("Program has finished. Press enter to quit.", Color.Aquamarine);
+            string greeting = Properties.Settings.Default.GreetingMessage;
+            string angryMessage = Properties.Settings.Default.AngryMessage;
+            Console.WriteLine(greeting);
+            string tiredMessage = ConfigurationManager.AppSettings["tiredMessage"];
+            Console.WriteLine(tiredMessage);
+            Console.ReadKey();
+
+        }
+
+    }
+}
